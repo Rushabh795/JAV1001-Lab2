9b5bb04549c69c243b14a0df9d0f8e88a274ca72
Aman <thakur.aman77@yahoo.com>
2022-06-06 21:22:42 -0400

    Added comments

--- ArrayTools.java
+++ ArrayTools.java
@@ -14,19 +14,28 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-
 import javax.lang.model.util.ElementScanner6;
 
+// This class contains all the bussiness and functioal logic
 public class ArrayTools {
+    // Initilased a scanner for input purposes throughout the class
+    //
     static Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        // Called start method to start the program execution
+        //
         start();
     }
 
+    // This method is where execution begins
+    //
     static void start() {
+        // Asking for input whether user wants to do:
+        // String Encryption or use Array Tools (Reverse, Average, Find Element)
+        //
         System.out.println(
-                "Please select an option to perform:- \n1. Caesar Cipher.\n2. Array Tools (Reverse, Average, Find).\n3. Exit.");
+                "Please select an option to perform:- \n1. Caesar Cipher.\n2. Array Tools (Reverse an array, Find average of array elements, Find an element from array).\n3. Exit.");
         int option = myScanner.nextInt();
         if (option == 1) {
             stringCipher();
@@ -35,11 +44,16 @@ public class ArrayTools {
         } else if (option == 3) {
             // Exit
         } else {
+            // In case user enters option which is not present
+            //
             System.out.println("Please try again!");
             start();
         }
     }
 
+    // This is asked once user done with one operation
+    // Y: asks for more or other operation
+    // N: Exit
     static void tryMore() {
         System.out.println("Do you wish to perform more actions? (Y/N)");
         String option = myScanner.next().toUpperCase();
@@ -50,6 +64,8 @@ public class ArrayTools {
         }
     }
 
+    // This method asks user string input
+    //
     static void stringCipher() {
         System.out.println("Please enter string for Caesar cipher");
         String normalString = myScanner.nextLine();
@@ -61,6 +77,8 @@ public class ArrayTools {
         tryMore();
     }
 
+    // This method used for array manipulations
+    //
     static void arrayTools(boolean myValue, int[] myArr) {
         if (myValue) {
             System.out.println("How many elements you want to enter in the Array?");
@@ -122,6 +140,9 @@ public class ArrayTools {
         }
     }
 
+    // This method encrypts string with help of Caesar Cipher
+    // Takes string and offset to convert
+    //
     public static String cypher(String message, int offset) {
         String result = new String();
         for (char character : message.toCharArray()) {
@@ -137,6 +158,8 @@ public class ArrayTools {
         return result;
     }
 
+    // To find average from array passed to function
+    //
     public static double getAverageFromArray(int[] array) {
         int arrayTotal = 0;
         for (int i = 0; i < array.length; i++) {
@@ -146,6 +169,8 @@ public class ArrayTools {
         return average;
     }
 
+    // To find reverse of an array passed to function
+    //
     public static int[] getReversedArray(int[] array) {
         int[] reversed = new int[array.length];
         for (int i = array.length; i > 0; i--) {
@@ -154,6 +179,9 @@ public class ArrayTools {
         return reversed;
     }
 
+    // Find an item from the array passed
+    // Accepts item from user
+    //
     public static boolean findNumber(int[] array, int numberToFind) {
         boolean found = false;
         for (int i = 0; i < array.length; i++) {
@@ -165,6 +193,8 @@ public class ArrayTools {
         return found;
     }
 
+    // Converts array list to integer array
+    //
     public static int[] convertNow(List<Integer> intList) {
         int arr[] = new int[intList.size()];
         Iterator i1 = intList.iterator();
@@ -174,6 +204,9 @@ public class ArrayTools {
         return arr;
     }
 
+    // This method asks for more options
+    // Such as new array or return to main menu
+    //
     static void getConvertmore(int[] myArr) {
         System.out.println('\n' + "=========================================");
         System.out.println("Do you want to countinue with" + '\n'
